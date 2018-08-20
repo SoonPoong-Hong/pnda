4e454f5c23024aa5d02e1ceb9dffc12433f2384e
trsmith2 <trsmith2@cisco.com>
2018-08-20 12:38:47 -0500

    Experiment with clean chrooted mirror

--- mirror/create_mirror_rpm.sh
+++ mirror/create_mirror_rpm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ev
+CHROOT=/clean/chroot
+
 export DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
 
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
@@ -22,41 +24,37 @@ SALT_REPO_KEY2=http://repo.saltstack.com/yum/redhat/7/x86_64/${SALTSTACK_REPO}/b
 [[ -z ${AMBARI_LEGACY_REPO} ]] && export AMBARI_LEGACY_REPO=http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/${AMBARI_LEGACY_VERSION}/ambari.repo
 [[ -z ${AMBARI_REPO_KEY} ]] && export AMBARI_REPO_KEY=http://public-repo-1.hortonworks.com/ambari/centos7/RPM-GPG-KEY/RPM-GPG-KEY-Jenkins
 
-yum install -y $RPM_EPEL || true
-yum install -y yum-utils
-yum-config-manager --enable $RPM_EXTRAS $RPM_OPTIONAL
-yum-config-manager --add-repo $MY_SQL_REPO
-yum-config-manager --add-repo $CLOUDERA_MANAGER_REPO
-yum-config-manager --add-repo $SALT_REPO
-yum-config-manager --add-repo $AMBARI_REPO
+yum --installroot $CHROOT install -y $RPM_EPEL || true
+yum --installroot $CHROOT install -y yum-utils
+yum-config-manager --installroot $CHROOT --enable $RPM_EXTRAS $RPM_OPTIONAL
+yum-config-manager --installroot $CHROOT --add-repo $MY_SQL_REPO
+yum-config-manager --installroot $CHROOT --add-repo $CLOUDERA_MANAGER_REPO
+yum-config-manager --installroot $CHROOT --add-repo $SALT_REPO
+yum-config-manager --installroot $CHROOT --add-repo $AMBARI_REPO
 
-curl -LJ -o /etc/yum.repos.d/ambari-legacy.repo $AMBARI_LEGACY_REPO
+cp /etc/yum.repos.d/* $CHROOT/etc/yum.repos.d/
+curl -LJ -o $CHROOT/etc/yum.repos.d/ambari-legacy.repo $AMBARI_LEGACY_REPO
 
 rm -rf $RPM_REPO_DIR
 mkdir -p $RPM_REPO_DIR
 
 cd $RPM_REPO_DIR
-cp /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7 $RPM_REPO_DIR
-if [ "x$DISTRO" == "xrhel" ]; then
-	# Not present on CentOS
-	cp /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release $RPM_REPO_DIR
-fi
 curl -LOJf $MY_SQL_REPO_KEY
 curl -LOJf $CLOUDERA_MANAGER_REPO_KEY
 curl -LOJf $SALT_REPO_KEY
 curl -LOJf $SALT_REPO_KEY2
 curl -LOJf $AMBARI_REPO_KEY
+curl -LOJf https://archive.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
 
 # import repo keys
-rpm --import *
-
+rpm --root $CHROOT --import *
 
 yum install -y createrepo
 
 #TODO yumdownloader doesn't always seem to download the full set of packages, for instance if git is installed, it won't download perl
 # packages correctly maybe because git already installed them. repotrack is meant to be better but I couldn't get that working.
 # yumdownloader also doesn't set its exit code when a package is not found, so this scans the log output for this case and manually exits with an error
-(yumdownloader --resolve --archlist=x86_64 --destdir $RPM_REPO_DIR $RPM_PACKAGE_LIST 2>&1) | tee -a yum-downloader.log; cmd_result=${PIPESTATUS[0]} && if [ ${cmd_result} != '0' ]; then exit ${cmd_result}; fi
+(yumdownloader --installroot $CHROOT --resolve --archlist=x86_64 --destdir $RPM_REPO_DIR $RPM_PACKAGE_LIST 2>&1) | tee -a yum-downloader.log; cmd_result=${PIPESTATUS[0]} && if [ ${cmd_result} != '0' ]; then exit ${cmd_result}; fi
 if grep -q 'No Match for argument' "yum-downloader.log"; then
     echo "missing rpm detected:"
     echo $(cat yum-downloader.log | grep 'No Match for argument')
